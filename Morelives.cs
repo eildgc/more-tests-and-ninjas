ddd57c640c35b352019a0582da457842c0fbbfab
Ely Gandara <ely.gandara.c@gmail.com>
2020-03-08 23:10:52 -0700

    Morelives file added.

---
+++ Morelives.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace more_tests_and_ninjas {
+    //Hace todo lo que hace la interfaz target mas otras cosas que haremos aqui
+    interface MoreLives : Target {
+        void Hp();
+    }
+}
