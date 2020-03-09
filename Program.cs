7bdc4a595d543320d717bbbe0a09e143d312c6e5
Ely Gandara <ely.gandara.c@gmail.com>
2020-03-08 20:01:04 -0700

    Added basic files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace more_tests_and_ninjas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
